435b5055af77befa4fd1edf966471031716262d0
Marian Smolii <smoliymaryan89@gmail.com>
2024-10-23 14:42:31 +0300

    feat(redux): add redux provider to the app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,19 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./scss/main.scss";
 
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { persistor, store } from "./store/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </StrictMode>
 );
